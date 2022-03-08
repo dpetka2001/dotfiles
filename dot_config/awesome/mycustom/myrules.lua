e11b0bd0e97a31748836e84793011f3a0cbb00ee
Iordanis Petkakis <dpetka2001@gmail.com>
2022-03-08 20:54:20 +0200

    Change wm_class for firefox in my rules

--- dot_config/awesome/mycustom/myrules.lua
+++ dot_config/awesome/mycustom/myrules.lua
@@ -6,7 +6,7 @@ myRules = {
 -- Set terminal to always map on the tag named "2" on screen 1.
 { rule = { class = term },
     properties = { screen = 1, tag = "2", switchtotag = true } },
-{ rule = { instance = "Navigator", class = "Firefox" },
+{ rule = { instance = "Navigator", class = "firefox" },
     properties = { screen = 1, tag = "1", switchtotag = true } },
 }
 
