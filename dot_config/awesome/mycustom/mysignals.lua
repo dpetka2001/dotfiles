608c860506f56db268191f9758cc368635652a62
Iordanis Petkakis <dpetka2001@gmail.com>
2022-08-02 15:32:22 +0300

    Stuff stuff

--- dot_config/awesome/mycustom/mysignals.lua
+++ dot_config/awesome/mycustom/mysignals.lua
@@ -1,35 +1,35 @@
 local awful = require('awful')
 
 client.connect_signal("unmanage", function(c)
-    local t = c.first_tag or awful.screen.focused().selected_tag
-    -- Check if tag is empty of clients
-    for _, cl in ipairs(t:clients()) do
-      if cl ~= c then
+  local t = c.first_tag or awful.screen.focused().selected_tag
+  -- Check if tag is empty of clients
+  for _, cl in ipairs(t:clients()) do
+    if cl ~= c then
+      return
+    end
+  end
+
+  -- Go to previously accessed tag and check if there are any clients, else
+  -- proceed with the `for` loop which checks each tag for open clients from
+  -- the beginning. We do this because if we execute the `for` loop
+  -- immediately, it will check from the beginning and return the first tag
+  -- with open clients, thus potentially skipping the previously accessed tag
+  -- if it has any open clients still. This is just a personal preference.
+  awful.tag.history.restore(mouse.screen)
+  t1 = awful.screen.focused().selected_tag
+  if #t1:clients() > 0 then
+    t1:view_only()
+    return
+  else
+    -- Check to see the first tag that has active clients to switch to (we do this by
+    -- checking if length of table tag:clients() is not nil.
+    -- Reverse tag history again to avoid switching between empty tags
+    awful.tag.history.restore(mouse.screen)
+    for _, t in ipairs(awful.screen.focused().tags) do
+      if #t:clients() > 0 then
+        t:view_only()
         return
       end
     end
-
-    -- Go to previously accessed tag and check if there are any clients, else
-    -- proceed with the `for` loop which checks each tag for open clients from
-    -- the beginning. We do this because if we execute the `for` loop
-    -- immediately, it will check from the beginning and return the first tag
-    -- with open clients, thus potentially skipping the previously accessed tag
-    -- if it has any open clients still. This is just a personal preference.
-    awful.tag.history.restore(mouse.screen)
-    t1 = awful.screen.focused().selected_tag
-    if #t1:clients() > 0 then
-      t1:view_only()
-      return
-    else
-      -- Check to see the first tag that has active clients to switch to (we do this by
-      -- checking if length of table tag:clients() is not nil.
-      -- Reverse tag history again to avoid switching between empty tags
-      awful.tag.history.restore(mouse.screen)
-      for _, t in ipairs(awful.screen.focused().tags) do
-        if #t:clients() > 0 then
-          t:view_only()
-          return
-        end
-      end
-    end
+  end
 end)
--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -512,7 +512,7 @@ awful.rules.rules = {
                      buttons = clientbuttons,
                      screen = awful.screen.preferred,
                      placement = awful.placement.no_overlap+awful.placement.no_offscreen
-     }
+      },
     },
 
     -- Floating clients.
