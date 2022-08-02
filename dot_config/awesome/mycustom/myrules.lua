88b80f1f5a52715971ceeb9ac4713ef4cbb86a42
Iordanis Petkakis <dpetka2001@gmail.com>
2022-08-02 15:32:22 +0300

    Stuff stuff

--- dot_config/awesome/mycustom/myrules.lua
+++ dot_config/awesome/mycustom/myrules.lua
@@ -3,11 +3,20 @@
 local term = "kitty"
 
 myRules = {
--- Set terminal to always map on the tag named "2" on screen 1.
-{ rule = { class = term },
-    properties = { screen = 1, tag = "2", switchtotag = true } },
-{ rule = { instance = "Navigator", class = "firefox" },
-    properties = { screen = 1, tag = "1", switchtotag = true } },
+  -- Set terminal to always map on the tag named "2" on screen 1.
+  {
+    rule = { class = term },
+    properties = { screen = 1, tag = "2", switchtotag = true }
+  },
+  {
+    rule_any = { name = {"Mozilla Firefox (Private Browsing)"} },
+    properties = { screen = 1, tag = "3", switchtotag = true },
+  },
+  {
+    rule_any = { class = {"firefox"} },
+    except_any = { name = {"Mozilla Firefox (Private Browsing)"} },
+    properties = { screen = 1, tag = "1", switchtotag = true }
+  },
 }
 
 return myRules
--- dot_config/nvim/lua/jrn23/lsp/lualsp.lua
+++ dot_config/nvim/lua/jrn23/lsp/lualsp.lua
@@ -7,32 +7,44 @@ table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 require'lspconfig'.sumneko_lua.setup {
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
-    },
-    cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"};
-    settings = {
-      Lua = {
-        runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-            -- Setup your lua path
-            path = runtime_path,
-        },
-        diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = {'vim'},
-            disable = { 'lowercase-global', 'unused-local', 'duplicate-index', 'undefined-global' },
-        },
-        workspace = {
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-            enable = false,
-        },
+  on_attach = on_attach,
+  flags = {
+    debounce_text_changes = 150,
+  },
+  cmd = {sumneko_binary_path, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
       },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          -- Awesome WM related
+          'awesome',
+          'client',
+          'root'
+        },
+        disable = { 'lowercase-global', 'unused-local', 'duplicate-index', 'undefined-global' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+            '/usr/share/nvim/runtime/lua',
+            '/usr/share/nvim/runtime/lua/lsp',
+            '/usr/share/awesome/lib'
+        -- vim.api.nvim_get_runtime_file("", true),
+        -- This is for Awesome WM Lua API
+      }
     },
+    -- Do not send telemetry data containing a randomized but unique identifier
+    telemetry = {
+      enable = false,
+    },
+  },
+},
 }
