ac6b58d7791b172b2de39e6c708260747e1e6da1
Iordanis Petkakis <dpetka2001@gmail.com>
2022-08-16 01:34:28 +0300

    Nvim-cmp changes

--- dot_config/nvim/lua/jrn23/completion/nvim-cmp.lua
+++ dot_config/nvim/lua/jrn23/completion/nvim-cmp.lua
@@ -15,20 +15,20 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
+    ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
+      behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     })
   },
 
   -- You should specify your *installed* sources.
-  sources = {
+  sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'path' },
     { name = 'nvim-lua' },
     { name = 'buffer' },
-  },
+  }),
 
   -- Make it auto-complete like nvim-compe
   completion = {
