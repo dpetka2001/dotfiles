cd243398e6a52a28a6acae8d4c628cf76b30471c
Iordanis Petkakis <dpetka2001@gmail.com>
2022-10-04 14:53:33 +0300

    Revert cmp-nvim-lsp-document-symbol changes

--- dot_config/nvim/lua/jrn23/completion/nvim-cmp.lua
+++ dot_config/nvim/lua/jrn23/completion/nvim-cmp.lua
@@ -64,14 +64,14 @@ cmp.setup {
 --   }
 -- })
 
-cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp_document_symbol' }
-  }, {
-    { name = 'buffer' }
-  })
-})
+-- cmp.setup.cmdline('/', {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = cmp.config.sources({
+--     { name = 'nvim_lsp_document_symbol' }
+--   }, {
+--     { name = 'buffer' }
+--   })
+-- })
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
