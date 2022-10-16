33f3acc6bd96454508474d46abe73cb48fcf88c1
Iordanis Petkakis <dpetka2001@gmail.com>
2022-10-16 20:59:05 +0300

    Kitty theme changes and capabilities of nvim-cmp-lsp
    Change the kitty theme because of difficult colors in ipython shell.

--- dot_config/nvim/lua/jrn23/completion/nvim-cmp.lua
+++ dot_config/nvim/lua/jrn23/completion/nvim-cmp.lua
@@ -79,7 +79,7 @@ cmp.setup.cmdline('?', {
 
 -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- The following example advertise capabilities to `clangd`.
 require'lspconfig'.clangd.setup {
