7ebfe7980160b3ba1bfdc99b4a88669bb68a18dd
Iordanis Petkakis <dpetka2001@gmail.com>
2023-01-15 14:28:54 +0200

    Some further customization regarding fish abbrs

---
+++ dot_config/private_fish/conf.d/abbrs.fish
@@ -0,0 +1,9 @@
+abbr -a gc git commit -m
+abbr -a gl git\ log\ --color\ --graph\ --date=format:\'\%Y-\%m-\%d\ \%H:\%M:\%S\'\ --pretty=format:\'\%Cred\%h\%Creset\ -\%C\(yellow\)\%d\%Creset\ \%s\ \%Cgreen\(\%ad\)\ \%C\(bold\ blue\)\<\%an\>\%Creset\'
+abbr -a ga git add -u .
+abbr -a gch git checkout
+abbr -a gr git reflog
+abbr -a gsh git show
+abbr -a gs git status
+abbr -a gp git push -u origin main
+abbr -a gd git diff
--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -13,14 +13,3 @@ set -gx LC_CTYPE en_US.UTF-8
 set -gx LC_NUMERIC en_US.UTF-8
 
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
-
-# Abrreviations
-abbr -a gc git commit -m
-abbr -a gl git\ log\ --color\ --graph\ --date=format:\'\%Y-\%m-\%d\ \%H:\%M:\%S\'\ --pretty=format:\'\%Cred\%h\%Creset\ -\%C\(yellow\)\%d\%Creset\ \%s\ \%Cgreen\(\%ad\)\ \%C\(bold\ blue\)\<\%an\>\%Creset\'
-abbr -a ga git add -u .
-abbr -a gch git checkout
-abbr -a gr git reflog
-abbr -a gsh git show
-abbr -a gs git status
-abbr -a gp git push -u origin main
-abbr -a gd git diff
