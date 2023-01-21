c5a90417b15bb580fa1501caa9dc6cd4c0e3a7e1
Iordanis Petkakis <dpetka2001@gmail.com>
2023-01-21 15:27:35 +0200

    Add `symbols-outline.nvim`

---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,11 @@
+return {
+
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = {
+      { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" },
+    },
+    config = true,
+  },
+}
