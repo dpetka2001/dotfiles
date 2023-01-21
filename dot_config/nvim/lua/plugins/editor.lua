c8f9d891c53737bb07b1864942807e25b8e25ffc
Iordanis Petkakis <dpetka2001@gmail.com>
2023-01-22 01:25:02 +0200

    Add rnvimr file manager and change some stuff accordingly

---
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,42 @@
+return {
+
+  -- disable neo-tree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+
+  -- use nvim-tree instead
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    version = "nightly",
+    cmd = "NvimTreeToggle",
+    keys = {
+      { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Nvim-tree" },
+      {
+        "<leader>E",
+        "<cmd>lua require('nvim-tree.api').tree.change_root_to_node()<cr>",
+        desc = "Nvim-tree change root to node",
+      },
+    },
+    opts = {
+      live_filter = {
+        prefix = "[FILTER]: ",
+        always_show_folders = false, -- Turn into false from true by default
+      },
+      sync_root_with_cwd = true,
+      respect_buf_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+        ignore_list = { "help" },
+      },
+      view = {
+        centralize_selection = true,
+      },
+    },
+  },
+}
---
+++ dot_config/nvim/lua/plugins/rnvimr.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "kevinhwang91/rnvimr",
+    -- cmd = "RnvimrToggle",
+    -- keys = {
+    --   { "<leader>T", "<cmd>RnvimrToggle<cr>", desc = "Rnvimr" },
+    -- },
+  },
+}
