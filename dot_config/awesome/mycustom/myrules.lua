d55ed251f491a25ed626e925081f270b8dc12bec
Iordanis Petkakis <dpetka2001@gmail.com>
2023-01-26 21:06:15 +0200

    Change Awesome myrules for Brave and remove KeepassXC from chezmoi managed

--- dot_config/awesome/mycustom/myrules.lua
+++ dot_config/awesome/mycustom/myrules.lua
@@ -3,20 +3,21 @@
 local term = "kitty"
 
 myRules = {
-  -- Set terminal to always map on the tag named "2" on screen 1.
-  {
-    rule = { class = term },
-    properties = { screen = 1, tag = "term", switchtotag = true }
-  },
-  {
-    rule_any = { name = {"Mozilla Firefox (Private Browsing)"} },
-    properties = { screen = 1, tag = "misc", switchtotag = true },
-  },
-  {
-    rule_any = { class = {"firefox"} },
-    except_any = { name = {"Mozilla Firefox (Private Browsing)"} },
-    properties = { screen = 1, tag = "www", switchtotag = true }
-  },
+	-- Set terminal to always map on the tag named "2" on screen 1.
+	{
+		rule = { class = term },
+		properties = { screen = 1, tag = "term", switchtotag = true },
+	},
+	{
+		-- rule_any = { name = {"Mozilla Firefox (Private Browsing)"} },
+		rule_any = { name = { "New Private Tab - Brave" } },
+		properties = { screen = 1, tag = "misc", switchtotag = true },
+	},
+	{
+		rule_any = { class = { "firefox" } },
+		except_any = { name = { "Mozilla Firefox (Private Browsing)" } },
+		properties = { screen = 1, tag = "www", switchtotag = true },
+	},
 }
 
 return myRules
