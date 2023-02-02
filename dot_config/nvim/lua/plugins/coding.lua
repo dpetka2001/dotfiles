11794193c787d4121276fbefc651d4ee4dd5bb26
Iordanis Petkakis <dpetka2001@gmail.com>
2023-02-02 20:31:19 +0200

    Add `menu` to `nvim-cmp` to show source names

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -4,6 +4,7 @@ return {
     "hrsh7th/nvim-cmp",
     opts = function(_, opts)
       local cmp = require("cmp")
+
       opts.mapping = cmp.mapping.preset.insert(vim.tbl_deep_extend("force", opts.mapping, {
         ["<CR>"] = cmp.mapping.confirm({
           behavior = cmp.ConfirmBehavior.Insert,
@@ -14,6 +15,19 @@ return {
           select = true,
         }),
       }))
+      opts.formatting.format = function(entry, item)
+        local icons = require("lazyvim.config").icons.kinds
+        if icons[item.kind] then
+          item.kind = icons[item.kind] .. item.kind
+        end
+        item.menu = ({
+          nvim_lsp = "[LSP]",
+          buffer = "[Buffer]",
+          luasnip = "[LuaSnip]",
+          path = "[Path]",
+        })[entry.source.name]
+        return item
+      end
     end,
   },
 }
