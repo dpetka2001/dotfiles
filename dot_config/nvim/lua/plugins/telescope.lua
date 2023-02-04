20a0565ddf2d5592b7197cc31e7e7473d0294a08
Iordanis Petkakis <dpetka2001@gmail.com>
2023-02-04 20:22:41 +0200

    Add multi-file open support for Telescope pickers

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,6 @@
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -8,10 +11,32 @@ return {
             ["<C-b>"] = function(...)
               return require("telescope.actions").delete_buffer(...)
             end,
+            -- Add opening multi-selection support to telescope pickers
+            ["<CR>"] = function(pb)
+              local picker = action_state.get_current_picker(pb)
+              local multi = picker:get_multi_selection()
+              actions.select_default(pb) -- the normal enter behaviour
+              for _, j in pairs(multi) do
+                if j.path ~= nil then -- is it a file -> open it as well:
+                  vim.cmd(string.format("%s %s", "edit", j.path))
+                end
+              end
+            end,
           },
           n = {
+            -- Add opening multi-selection support to telescope pickers
             ["<C-b>"] = function(...)
               return require("telescope.actions").delete_buffer(...)
+            end,
+            ["<CR>"] = function(pb)
+              local picker = action_state.get_current_picker(pb)
+              local multi = picker:get_multi_selection()
+              actions.select_default(pb) -- the normal enter behaviour
+              for _, j in pairs(multi) do
+                if j.path ~= nil then -- is it a file -> open it as well:
+                  vim.cmd(string.format("%s %s", "edit", j.path))
+                end
+              end
             end,
           },
         },
