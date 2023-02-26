228247cbe355dcc9ccf22de21c54516b68876844
Iordanis Petkakis <dpetka2001@gmail.com>
2023-02-26 13:36:11 +0200

    Changes to modify `navic` bg_color to be same as `lualine`

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -65,6 +65,46 @@ autocmd("TermOpen", {
   desc = "Set terminal mappings for ToggleTerm",
 })
 
+-- Colorscheme
+autocmd("Colorscheme", {
+  pattern = "*",
+  callback = function()
+    vim.cmd([[
+      hi link NavicText lualine_c_normal
+      hi link NavicIconsFile lualine_c_normal
+      hi link NavicIconsModule lualine_c_normal
+      hi link NavicIconsNamespace lualine_c_normal
+      hi link NavicIconsPackage lualine_c_normal
+      hi link NavicIconsClass lualine_c_normal
+      hi link NavicIconsMethod lualine_c_normal
+      hi link NavicIconsProperty lualine_c_normal
+      hi link NavicIconsField lualine_c_normal
+      hi link NavicIconsConstructor lualine_c_normal
+      hi link NavicIconsEnum lualine_c_normal
+      hi link NavicIconsInterface lualine_c_normal
+      hi link NavicIconsFunction lualine_c_normal
+      hi link NavicIconsVariable lualine_c_normal
+      hi link NavicIconsConstant lualine_c_normal
+      hi link NavicIconsString lualine_c_normal
+      hi link NavicIconsNumber lualine_c_normal
+      hi link NavicIconsBoolean lualine_c_normal
+      hi link NavicIconsArray lualine_c_normal
+      hi link NavicIconsObject lualine_c_normal
+      hi link NavicIconsKey lualine_c_normal
+      hi link NavicIconsNull lualine_c_normal
+      hi link NavicIconsEnumMember lualine_c_normal
+      hi link NavicIconsStruct lualine_c_normal
+      hi link NavicIconsEvent lualine_c_normal
+      hi link NavicIconsOperator lualine_c_normal
+      hi link NavicIconsTypeParameter lualine_c_normal
+      hi link NavicText lualine_c_normal
+      hi link NavicSeparator lualine_c_normal
+    ]])
+  end,
+  group = "mygroup",
+  desc = "Set highlight groups for navic",
+})
+
 --[[ User Commands ]]
 
 usercmd("Rwd", function()
--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,3 +1,5 @@
+local icons = require("lazyvim.config").icons
+
 return {
   -- Modify nvim-notify for Telescope
   {
@@ -29,6 +31,35 @@ return {
     opts = {
       options = {
         always_show_bufferline = true,
+      },
+    },
+  },
+
+  -- Modify `lualine`
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      sections = {
+        lualine_c = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+          -- stylua: ignore
+          {
+            function() return require("nvim-navic").get_location() end,
+            cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
+            padding = { left =1, right = 0 },
+          },
+        },
       },
     },
   },
