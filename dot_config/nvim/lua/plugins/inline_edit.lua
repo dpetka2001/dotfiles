0d509a9ce3dad7fafc6083e1c4f7ac81fad73d38
Iordanis Petkakis <dpetka2001@gmail.com>
2023-03-17 21:07:01 +0200

    Add `inline_edit.vim`
    Provides support for embedded languages inside Neovim via proxy
    buffer editing and correctly attaching corresponding LSP server.

---
+++ dot_config/nvim/lua/plugins/inline_edit.lua
@@ -0,0 +1,12 @@
+return {
+  -- Plugin for editing <script> tags in html files in proxy buffer
+  -- which also provides support for correct attach `typescript` LSP
+  {
+    "AndrewRadev/inline_edit.vim",
+    cmd = { "InlineEdit" },
+    keys = {
+      { "<leader>cI", "<cmd>InlineEdit<cr>", desc = "Inline Edit" },
+    },
+    config = true,
+  },
+}
