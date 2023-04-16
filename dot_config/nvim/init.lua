022088e4e53fa23e892698aee9b74d133335547b
Iordanis Petkakis <dpetka2001@gmail.com>
2023-04-16 23:33:02 +0300

    Add `dap` protocol and adapter for Lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+-- Add this here to disable lazyvim default options if you want
+-- package.loaded["lazyvim.config.options"] = true
+
 require("config.lazy")
--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -14,6 +14,8 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
     -- import/override with your plugins
     { import = "plugins" },
   },
