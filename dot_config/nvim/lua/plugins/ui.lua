7982b864217eb99fbb0889bffa2e70df042cd98d
Iordanis Petkakis <dpetka2001@gmail.com>
2023-04-25 13:41:06 +0300

    feat(ui): add `encoding` to lualine_x

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -64,4 +64,11 @@ return {
       },
     },
   },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, { "encoding" })
+    end,
+  },
 }
