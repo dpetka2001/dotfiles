a6974e03d40edae5a7ddf00dc5fe28ce9f0b6b05
Iordanis Petkakis <dpetka2001@gmail.com>
2023-05-02 10:38:58 +0300

    chore: update lockfile

--- dot_config/private_fish/functions/fish_title.fish
+++ dot_config/private_fish/functions/fish_title.fish
@@ -2,6 +2,6 @@ function fish_title
     set -q argv[1]; or set argv fish
     # Looks like ~/d/fish: git log
     # or /e/apt: fish
-    # echo (fish_prompt_pwd_dir_length=1 prompt_pwd): $argv;
-    echo $argv;
+    # echo (fish_prompt_pwd_dir_length=1 prompt_pwd): (string split ' ' $argv[1] | read)
+    echo $argv
 end
