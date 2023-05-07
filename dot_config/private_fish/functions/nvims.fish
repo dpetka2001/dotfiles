d9a6eddca1b8b63922ccf2503de6a65a61d2501e
Iordanis Petkakis <dpetka2001@gmail.com>
2023-05-08 00:59:22 +0300

    feat(fish): ssh_agent and nvims(nvim selector) functions added

---
+++ dot_config/private_fish/functions/nvims.fish
@@ -0,0 +1,9 @@
+function nvims
+    set items nvim lazyTest
+    set config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+    if test -z $config
+        echo "Nothing selected"
+        return 0
+    end
+    NVIM_APPNAME="$config" nvim -- $argv
+end
