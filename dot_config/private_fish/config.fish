33a3ebd645c520075955c25a00c255382e13a95d
Iordanis Petkakis <dpetka2001@gmail.com>
2023-05-08 14:28:28 +0300

    fix(fish): update some fzf config options and user bindings

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,17 +1,24 @@
 set -g fish_prompt_pwd_dir_length 2
 set -gx EDITOR nvim
 set -gx VISUAL nvim
-# set -gx FZF_DEFAULT_COMMAND "rg --hidden --files"
+
+#################
+# Configure fzf #
+#################
 set -gx FZF_DEFAULT_COMMAND "rg --files"
 set -gx FZF_DEFAULT_OPTS "--layout=reverse --bind='ctrl-r:reload(rg --hidden --files)' --header='"Press CTRL-R to reload\n\n"' --border -m \
 --bind 'ctrl-v:execute(nvim {+})' --bind 'ctrl-d:reload(find . -type d)'"
-set -gx FZF_CTRL_T_COMMAND "rg --files"
-bind \cs fzf
+# set -gx FZF_CTRL_T_COMMAND "rg --files"
+set -g FZF_CTRL_T_COMMAND "command find -L \$dir -type f 2> /dev/null | sed '1d; s#^\./##'"
 
+# Avoid loading ranger default config more than once
 set -gx RANGER_LOAD_DEFAULT_RC FALSE
+
+# Set locale
 set -gx LC_CTYPE en_US.UTF-8
 set -gx LC_NUMERIC en_US.UTF-8
 
+# Change MANPAGER
 set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
 # Start ssh-agent when starting fish shell
--- dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -1,7 +1,9 @@
 function fish_user_key_bindings
-  fzf_key_bindings
-  #fish_default_key_bindings -M insert
-  #fish_vi_key_bindings --no-erase insert
-  bind \ck 'history-token-search-backward'
-  bind \cj 'history-token-search-forward'
+    fzf_key_bindings
+    #fish_default_key_bindings -M insert
+    #fish_vi_key_bindings --no-erase insert
+    bind \ck history-token-search-backward
+    bind \cj history-token-search-forward
+    bind \cs fzf
+    bind \cn nvims
 end
