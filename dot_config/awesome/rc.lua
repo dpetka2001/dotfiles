d5c384de780af662b3a2ae2fb58ba2fbeba56286
Iordanis Petkakis <dpetka2001@gmail.com>
2023-05-17 12:22:04 +0300

    fix(awesome): change tag `doc` to `dev`

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -226,7 +226,7 @@ awful.screen.connect_for_each_screen(function(s)
 	set_wallpaper(s)
 
 	-- Each screen has its own tag table.
-	awful.tag({ "www", "term", "doc", "misc", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+	awful.tag({ "www", "term", "dev", "misc", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
 	-- Create a promptbox for each screen
 	s.mypromptbox = awful.widget.prompt()
