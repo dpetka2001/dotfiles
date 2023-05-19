d0fdc88f655defe9964bfecff8776a61ab141d6f
Iordanis Petkakis <dpetka2001@gmail.com>
2023-05-19 11:19:02 +0300

    fix(mini.ai): add treesitter workaround to avoid prob with same name nested tags

---
+++ dot_config/nvim/after/ftplugin/html.lua
@@ -0,0 +1,6 @@
+local spec_treesitter = require("mini.ai").gen_spec.treesitter
+vim.b.miniai_config = {
+  custom_textobjects = {
+    t = spec_treesitter({ a = "@function.outer", i = "@function.inner" }),
+  },
+}
