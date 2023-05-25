ae74617d77918ae84dc7a62294dd0edff53b0eb9
Iordanis Petkakis <dpetka2001@gmail.com>
2023-05-26 01:59:10 +0300

    feat(fish/tmux): customize how they set titles

--- dot_config/private_fish/functions/fish_title.fish
+++ dot_config/private_fish/functions/fish_title.fish
@@ -3,5 +3,6 @@ function fish_title
     # Looks like ~/d/fish: git log
     # or /e/apt: fish
     # echo (fish_prompt_pwd_dir_length=1 prompt_pwd): (string split ' ' $argv[1] | read)
-    echo $argv
+    echo (string split ' ' $argv[1] | read) (fish_prompt_pwd_dir_length=1 prompt_pwd)
+    # echo $argv[1]
 end
