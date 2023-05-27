085876832e8cb878aecc1ee1c30357a464e65266
Iordanis Petkakis <dpetka2001@gmail.com>
2023-05-27 19:17:38 +0300

    feat(nvim): set `eventignore` in options so that tmux doesn't show activity every time we leave from a window with Neovim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -10,7 +10,7 @@ g.VM_theme = "purplegray"
 
 -- Indent
 opt.autoindent = true -- Copy indent from current line when starting a new line (When <CR> or 'o' or 'O')
-opt.smarttab = true -- When on, a <Tab> in front of a line inserts blanks according to 'shiftwidth'.
+opt.smarttab = true -- When on, a <Tab> in front of a line inserts blanks according to 'shiftwidth'
 
 -- Set this for ToggleTerm terminals to not be discarded when closed
 opt.hidden = true
@@ -18,3 +18,6 @@ opt.hidden = true
 -- Set fold level, so that folds are open when entering buffer
 opt.foldlevel = 99
 opt.foldlevelstart = 99
+
+-- Set eventignore so that tmux does not show `activity` every time we change from a window with Neovim
+opt.eventignore = "FocusLost,Focusgained"
