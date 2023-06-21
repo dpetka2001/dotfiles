c3ce0c891db5f4f93703ada161f0a028fbaa596c
Iordanis Petkakis <dpetka2001@gmail.com>
2023-06-21 18:25:05 +0300

    fix(flash): change keymap to the required example from the repo

--- dot_config/nvim/lua/plugins/flash.lua
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -15,33 +15,11 @@ return {
             end,
           })
         end,
-        desc = "Flash next diagnostic",
+        desc = "Flash diagnostic at chosen target",
       },
       {
         "]F",
-        function()
-          require("flash").jump({
-            search = { multi_window = true, wrap = true },
-            highlight = { backdrop = true, label = { current = true } },
-            matcher = function(win)
-              local buf = vim.api.nvim_win_get_buf(win)
-              ---@param diag Diagnostic
-              return vim.tbl_map(function(diag)
-                return {
-                  pos = { diag.lnum + 1, diag.col },
-                  end_pos = { diag.end_lnum + 1, diag.end_col - 1 },
-                }
-              end, vim.diagnostic.get(buf))
-            end,
-            action = function(match, state)
-              vim.api.nvim_win_call(match.win, function()
-                vim.api.nvim_win_set_cursor(match.win, match.pos)
-                vim.diagnostic.open_float()
-                vim.api.nvim_win_set_cursor(match.win, state.pos)
-              end)
-            end,
-          })
-        end,
+        "<cmd>lua require('flash.plugins.diagnostics').show()<cr>",
         desc = "Flash All Diagnostics",
       },
     },
