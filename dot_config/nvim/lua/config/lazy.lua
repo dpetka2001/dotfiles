3511be2e5f01213f826ee335a61e2bf7bf366097
Iordanis Petkakis <dpetka2001@gmail.com>
2023-06-21 12:56:30 +0300

    feat(lazy): enable `extras.editor.flash` in favor of `leap` and `flit`

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
+    { import = "lazyvim.plugins.extras.editor.flash" },
     -- import/override with your plugins
     { import = "plugins" },
   },
