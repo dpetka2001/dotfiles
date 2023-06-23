1501ab93068fb6e00d9c532b80f89aa414c8bfa4
Iordanis Petkakis <dpetka2001@gmail.com>
2023-06-23 11:09:23 +0300

    fix(tools): move `peek` configuration to `tools`

--- dot_config/nvim/lua/plugins/tools.lua
+++ dot_config/nvim/lua/plugins/tools.lua
@@ -94,4 +94,35 @@ return {
     cmd = { "MurenToggle", "MurenOpen", "MurenClose", "MurenFresh", "MurenUnique" },
     config = true,
   },
+
+  -- markdown preview
+  {
+    "toppair/peek.nvim",
+    build = "deno task --quiet build:fast",
+    keys = {
+      {
+        "<leader>pp",
+        function()
+          local peek = require("peek")
+          if peek.is_open() then
+            peek.close()
+          else
+            peek.open()
+          end
+        end,
+        desc = "Peek (Markdown Preview)",
+      },
+    },
+    opts = { theme = "dark" },
+  },
+
+  -- which-key namespace
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>p"] = { name = "+peek" },
+      },
+    },
+  },
 }
