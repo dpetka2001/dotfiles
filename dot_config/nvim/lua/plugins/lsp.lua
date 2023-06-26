d27edf4a203dd1413e2b741971f104606d867b06
Iordanis Petkakis <dpetka2001@gmail.com>
2023-06-26 20:22:47 +0300

    feat(null-ls): add gitsigns code actions to null-ls

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -146,6 +146,12 @@ return {
         end,
       })
     end,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources, {
+        nls.builtins.code_actions.gitsigns,
+      })
+    end,
   },
 
   -- Import extra lsp languages configs
