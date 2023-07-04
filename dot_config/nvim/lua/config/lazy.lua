2c96ace0f7a36d7e00388eafd53b097223e50aa0
Iordanis Petkakis <dpetka2001@gmail.com>
2023-07-04 21:21:10 +0300

    feat(neotest): add `neotest` to skel template and configure Python to use it and some changes in Python config

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- dot_config/nvim/lua/plugins/extras/lang/python.lua
+++ dot_config/nvim/lua/plugins/extras/lang/python.lua
@@ -19,8 +19,8 @@ return {
       "mfussenegger/nvim-dap-python",
       -- stylua: ignore
       keys = {
-        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug test method" },
-        { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug class" },
+        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
+        { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class" },
       },
       config = function()
         local path = require("mason-registry").get_package("debugpy"):get_install_path()
@@ -50,6 +50,24 @@ return {
     },
   },
 
+  -- Setup `neotest`
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-python",
+    },
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          -- Here you can specify the settings for the adapter, i.e.
+          -- runner = "pytest",
+          -- python = ".venv/bin/python",
+        },
+      },
+    },
+  },
+
   -- Add `server` and setup lspconfig
   {
     "neovim/nvim-lspconfig",
@@ -58,6 +76,16 @@ return {
       servers = {
         -- pyright = {},
         ruff_lsp = {},
+      },
+      setup = {
+        ruff_lsp = function()
+          require("lazyvim.util").on_attach(function(client, _)
+            if client.name == "ruff_lsp" then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end)
+        end,
       },
     },
   },
@@ -68,11 +96,12 @@ return {
     opts = function(_, opts)
       local nls = require("null-ls")
       opts.sources = vim.list_extend(opts.sources, {
+        -- Order of formatters matters. They are used in order of appearance.
+        nls.builtins.formatting.ruff,
         nls.builtins.formatting.black,
         -- nls.builtins.formatting.black.with({
         --   extra_args = { "--preview" },
         -- }),
-        nls.builtins.formatting.ruff,
         -- nls.builtins.diagnostics.ruff,
       })
     end,
