d0515a095cea0a067dccdf42d1a2147a120da9af
Iordanis Petkakis <dpetka2001@gmail.com>
2023-07-13 10:57:13 +0300

    chore(lazy): remove `extras.editor.flash` as it got integrated as default in LazyVim

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -17,7 +17,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.test.core" },
     -- import/override with your plugins
