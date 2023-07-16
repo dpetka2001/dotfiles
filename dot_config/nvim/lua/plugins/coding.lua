bf23a034983c64fdbf378399885bec15fd534bbd
Iordanis Petkakis <dpetka2001@gmail.com>
2023-07-16 21:56:48 +0300

    feat(kitty/cmp): add kitty keymap for `session` and dynamic completion width for `cmp`
    Amend: added link to github issue.

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -59,6 +59,7 @@ return {
       }
       opts.formatting.format = function(entry, item)
         local icons = require("lazyvim.config").icons.kinds
+        local item = item
         if entry.source.name == "codeium" then
           item.kind = "   (Codeium)"
           if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
@@ -75,6 +76,41 @@ return {
           path = "[Path]",
           codeium = "[A.I.]",
         })[entry.source.name]
+
+        --[[ Make completion width dynamic according to window width ]]
+        -- See https://github.com/hrsh7th/nvim-cmp/discussions/609
+
+        -- Set the fixed width of the completion menu to 60 characters.
+        -- fixed_width = 20
+
+        -- Set 'fixed_width' to false if not provided.
+        fixed_width = fixed_width or false
+
+        -- Get the completion entry text shown in the completion window.
+        local content = item.abbr
+
+        -- Set the fixed completion window width.
+        if fixed_width then
+          vim.o.pumwidth = fixed_width
+        end
+
+        -- Get the width of the current window.
+        local win_width = vim.api.nvim_win_get_width(0)
+
+        -- Set the max content width based on either: 'fixed_width'
+        -- or a percentage of the window width, in this case 20%.
+        -- We subtract 10 from 'fixed_width' to leave room for 'kind' fields.
+        local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.25)
+
+        -- Truncate the completion entry text if it's longer than the
+        -- max content width. We subtract 3 from the max content width
+        -- to account for the "..." that will be appended to it.
+        if #content > max_content_width then
+          item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+        else
+          item.abbr = content .. (" "):rep(max_content_width - #content)
+        end
+
         -- return item
         return require("tailwindcss-colorizer-cmp").formatter(entry, item)
       end
