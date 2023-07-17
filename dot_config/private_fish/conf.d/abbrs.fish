9909d08fd75fc87fbfbc8a64fd90cc55a38fe40d
Iordanis Petkakis <dpetka2001@gmail.com>
2023-07-17 13:04:50 +0300

    feat(kitty): add some abbrs for `kitty_install` and change `git log`

--- dot_config/private_fish/conf.d/abbrs.fish
+++ dot_config/private_fish/conf.d/abbrs.fish
@@ -1,6 +1,6 @@
 # abbr -a gc --set-cursor git commit -m \"%\"
 abbr -a gc git commit -m
-abbr -a gl git\ log\ --color\ --graph\ --date=format:\'\%Y-\%m-\%d\ \%H:\%M:\%S\'\ --pretty=format:\'\%Cred\%h\%Creset\ -\%C\(yellow\)\%d\%Creset\ \%s\ \%Cgreen\(\%ad\)\ \%C\(bold\ blue\)\<\%an\>\%Creset\'
+abbr -a gl "git log --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset'"
 abbr -a ga git add
 abbr -a gch git checkout
 abbr -a gr git reflog
@@ -14,3 +14,4 @@ abbr -a nv nvim
 abbr -a cl chezmoi git -- log --color --graph --date=format:\'\%Y-\%m-\%d \%H:\%M:\%S\' --pretty=format:\'\%Cred\%h\%Creset -\%C\(yellow\)\%d\%Creset \%s \%Cgreen\(\%ad\) \%C\(bold blue\)\<\%an\>\%Creset\'
 abbr -a nv_build make CMAKE_BUILD_TYPE=RelWithDebInfo
 abbr -a nv_install "cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb"
+abbr -a kitty_install "curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin"
