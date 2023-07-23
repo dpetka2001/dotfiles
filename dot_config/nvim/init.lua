237928ea9357a1261c227d6714997e3915005f52
Iordanis Petkakis <dpetka2001@gmail.com>
2023-07-24 01:50:39 +0300

    chore(init): bring back `vim.loader` from old config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+-- Add this here to disable lazyvim default options if you want
+-- package.loaded["lazyvim.config.options"] = true
+
+vim.loader.enable()
 require("config.lazy")
