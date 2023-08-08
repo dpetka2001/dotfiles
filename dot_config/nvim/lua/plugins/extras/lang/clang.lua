4525b1dbc47903ec19239dd6dadf65f11c906991
Iordanis Petkakis <dpetka2001@gmail.com>
2023-08-09 01:57:50 +0300

    fix(clangd): Update setup from upstream
    Move `clangd_extensions` outside of `nvim-lspconfig` and implement all
    `opts` there instead of inside the `setup` because the extension stopped
    supporting setting up the LSP.

--- dot_config/nvim/lua/plugins/extras/lang/clang.lua
+++ dot_config/nvim/lua/plugins/extras/lang/clang.lua
@@ -25,61 +25,82 @@ return {
   --   },
   -- },
 
+  {
+    "p00f/clangd_extensions.nvim",
+    lazy = true,
+    config = function() end, -- avoid duplicate setup call because it also loads in line 102
+    opts = {
+      inlay_hints = {
+        inline = false,
+      },
+      ast = {
+        --These require codicons (https://github.com/microsoft/vscode-codicons)
+        role_icons = {
+          type = "",
+          declaration = "",
+          expression = "",
+          specifier = "",
+          statement = "",
+          ["template argument"] = "",
+        },
+        kind_icons = {
+          Compound = "",
+          Recovery = "",
+          TranslationUnit = "",
+          PackExpansion = "",
+          TemplateTypeParm = "",
+          TemplateTemplateParm = "",
+          TemplateParamObject = "",
+        },
+      },
+    },
+  },
   -- Add `server` and setup lspconfig
   {
     "neovim/nvim-lspconfig",
-    dependencies = {
-      "p00f/clangd_extensions.nvim",
-      dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-      },
-    },
     opts = {
       servers = {
-        clangd = {},
-      },
-      setup = {
-        clangd = function(_, opts)
-          opts.capabilities.offsetEncoding = { "utf-16" }
-          opts.cmd = {
+        clangd = {
+          keys = {
+            { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+          },
+          root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+              "Makefile",
+              "configure.ac",
+              "configure.in",
+              "config.h.in",
+              "meson.build",
+              "meson_options.txt",
+              "build.ninja"
+            )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+              fname
+            ) or require("lspconfig.util").find_git_ancestor(fname)
+          end,
+          capabilities = {
+            offsetEncoding = { "utf-16" },
+          },
+          cmd = {
             "clangd",
             "--background-index",
             "--clang-tidy",
             "--header-insertion=iwyu",
             "--completion-style=detailed",
             "--function-arg-placeholders",
-          }
-          opts.init_options = {
+            "--fallback-style=llvm",
+          },
+          init_options = {
             usePlaceholders = true,
             completeUnimported = true,
             clangdFileStatus = true,
-          }
-          require("clangd_extensions").setup({
-            server = opts,
-            extensions = {
-              ast = {
-                --These require codicons (https://github.com/microsoft/vscode-codicons)
-                role_icons = {
-                  type = "",
-                  declaration = "",
-                  expression = "",
-                  specifier = "",
-                  statement = "",
-                  ["template argument"] = "",
-                },
-                kind_icons = {
-                  Compound = "",
-                  Recovery = "",
-                  TranslationUnit = "",
-                  PackExpansion = "",
-                  TemplateTypeParm = "",
-                  TemplateTemplateParm = "",
-                  TemplateParamObject = "",
-                },
-              },
-            },
-          })
-          return true
+          },
+        },
+      },
+      setup = {
+        clangd = function(_, opts)
+          local clangd_ext_opts = require("lazyvim.util").opts("clangd_extensions.nvim")
+          require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+          return false
         end,
       },
     },
