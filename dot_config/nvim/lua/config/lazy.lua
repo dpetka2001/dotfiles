0b48e5863a8bbc0a776267cb6ddf84a0dd2c01a8
Iordanis Petkakis <dpetka2001@gmail.com>
2023-08-08 21:17:14 +0300

    chore(lazy): change `checker.frequency`

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -32,7 +32,7 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = true, frequency = 14400 }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
