afb7c1b200d81e476d3dcd60392da8ec0f16e2dc
Iordanis Petkakis <dpetka2001@gmail.com>
2023-08-25 21:18:35 +0300

    feat(comments/treesitter): several changes to the modules
    - Treesitter: add `treesitter-context` and customize settings for
      Playground in treesitter `opts`
    - Comments: remove default settings from `Comment.nvim` `opts` and add
      `comment-box.nvim`

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,16 +3,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      -- Add Playground
-      {
-        "nvim-treesitter/playground",
-        cmd = "TSPlaygroundToggle",
-        keys = {
-          { "<leader>uth", "<cmd>TSHighlightCapturesUnderCursor<cr>", desc = "Show highlight groups under the cursor" },
-          { "<leader>utn", "<cmd>TSNodeUnderCursor<cr>", desc = "Show node under the cursor" },
-        },
-      },
-
       -- Add nvim-ts-autotag
       { "windwp/nvim-ts-autotag" },
 
@@ -26,6 +16,24 @@ return {
         "rust",
       })
       return vim.tbl_deep_extend("force", opts, {
+        playground = {
+          enable = true,
+          disable = {},
+          updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+          persist_queries = false, -- Whether the query persists across vim sessions
+          keybindings = {
+            toggle_query_editor = "o",
+            toggle_hl_groups = "i",
+            toggle_injected_languages = "t",
+            toggle_anonymous_nodes = "a",
+            toggle_language_display = "I",
+            focus_language = "f",
+            unfocus_language = "F",
+            update = "R",
+            goto_node = "<cr>",
+            show_help = "?",
+          },
+        },
         autotag = {
           enable = true,
         },
@@ -38,10 +46,34 @@ return {
           config = {
             c = { __default = "// %s", __multiline = "/* %s */" },
             cpp = { __default = "// %s", __multiline = "/* %s */" },
-            kdl = { __default = "// %s", __multiline = "/* %s */" },
           },
         },
       })
     end,
+  },
+
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSPlaygroundToggle",
+    keys = {
+      { "<leader>uth", "<cmd>TSHighlightCapturesUnderCursor<cr>", desc = "Show highlight groups under the cursor" },
+      { "<leader>utn", "<cmd>TSNodeUnderCursor<cr>", desc = "Show node under the cursor" },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "BufReadPre",
+    opts = { mode = "cursor" },
+    keys = {
+      {
+        "[C",
+        function()
+          require("treesitter-context").go_to_context()
+        end,
+        silent = true,
+        desc = "Go to Context",
+      },
+    },
   },
 }
