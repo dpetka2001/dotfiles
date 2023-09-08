0af81f07a8a82676b6423a36b7a1b8a51e5e286c
Iordanis Petkakis <dpetka2001@gmail.com>
2023-09-09 02:32:19 +0300

    perf(telescope): move code to `config` function for lazy loading
    `require`s and functions were being loaded when parsed initially by
    Lazy, so moved them to telescope's `config`.

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,41 +1,4 @@
-local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
-local builtins = require("telescope.builtin")
-local action_set = require("telescope.actions.set")
--- local my_actions = require("dpetka2001.harpoon")
-
 local Util = require("lazyvim.util")
-
-local multi_open = function(pb)
-  local picker = action_state.get_current_picker(pb)
-  local multi = picker:get_multi_selection()
-  -- my_actions.mark_file(pb)
-  actions.select_default(pb) -- the normal enter behaviour
-  for _, j in pairs(multi) do
-    if j.path ~= nil then -- is it a file -> open it as well:
-      vim.cmd(string.format("%s %s", "edit", j.path))
-    end
-  end
-end
-
-local picker_config = {}
-for builtin, _ in pairs(builtins) do
-  picker_config[builtin] = {
-    -- Don't show the matched line since it is already in the preview.
-    show_line = false,
-    -- Center and unfold when selecting a result.
-    attach_mappings = function(prompt_bufnr, _)
-      action_set.select:enhance({
-        post = function()
-          vim.cmd(":normal! zv")
-        end,
-      })
-      actions.center(prompt_bufnr)
-
-      return true
-    end,
-  }
-end
 
 return {
   {
@@ -54,74 +17,6 @@ return {
       { "natecraddock/telescope-zf-native.nvim" },
       { "debugloop/telescope-undo.nvim" },
     },
-    opts = {
-      defaults = {
-        mappings = {
-          i = {
-            ["<C-b>"] = function(...)
-              return require("telescope.actions").delete_buffer(...)
-            end,
-            -- Add opening multi-selection support to telescope pickers
-            ["<CR>"] = multi_open,
-            ["<c-a-t>"] = actions.select_tab,
-            ["<C-j>"] = "move_selection_next",
-            ["<C-k>"] = "move_selection_previous",
-            ["<Tab>"] = function(...)
-              return require("dpetka2001.harpoon").mark_file(...)
-            end,
-          },
-          n = {
-            ["<C-b>"] = function(...)
-              return require("telescope.actions").delete_buffer(...)
-            end,
-            -- Add opening multi-selection support to telescope pickers
-            ["<CR>"] = multi_open,
-            ["<Tab>"] = function(...)
-              return require("dpetka2001.harpoon").mark_file(...)
-            end,
-          },
-        },
-        layout_strategy = "flex",
-        layout_config = {
-          horizontal = {
-            preview_width = 0.45,
-          },
-          vertical = {
-            width = 0.9,
-            height = 0.95,
-            preview_height = 0.5,
-            preview_cutoff = 0,
-          },
-          flex = {
-            flip_columns = 140,
-          },
-        },
-      },
-      -- pickers = {
-      --   buffers = {
-      --     initial_mode = "normal",
-      --   },
-      -- },
-      pickers = vim.tbl_deep_extend("force", picker_config, {
-        -- Open Telescope even if there's only one result.
-        lsp_references = { jump_type = "never" },
-        lsp_definitions = { jump_type = "never" },
-        buffers = { initial_mode = "normal" },
-      }),
-      extensions = {
-        project = {
-          base_dirs = {
-            { path = "~/Desktop/Projects/" },
-            { path = "~/projects/plugins/" },
-            { path = "~/.local/share/chezmoi/" },
-            { path = "~/.local/share/nvim/" },
-          },
-        },
-        undo = {
-          side_by_side = true,
-        },
-      },
-    },
     keys = {
       { "<leader>sd", false },
       { "<leader>sD", false },
@@ -137,9 +32,101 @@ return {
       { "<leader>U", "<cmd>Telescope undo<cr>", desc = "Telescope undo" },
     },
     -- Setup here extensions that depend on `telescope.opts`, otherwise just setup when it is called
-    config = function(_, opts)
+    config = function()
       local telescope = require("telescope")
-      telescope.setup(opts)
+      local actions = require("telescope.actions")
+      local action_state = require("telescope.actions.state")
+      local builtins = require("telescope.builtin")
+      local action_set = require("telescope.actions.set")
+
+      local multi_open = function(pb)
+        local picker = action_state.get_current_picker(pb)
+        local multi = picker:get_multi_selection()
+        -- my_actions.mark_file(pb)
+        actions.select_default(pb) -- the normal enter behaviour
+        for _, j in pairs(multi) do
+          if j.path ~= nil then -- is it a file -> open it as well:
+            vim.cmd(string.format("%s %s", "edit", j.path))
+          end
+        end
+      end
+
+      local picker_config = {}
+      for builtin, _ in pairs(builtins) do
+        picker_config[builtin] = {
+          -- Don't show the matched line since it is already in the preview.
+          show_line = false,
+          -- Center and unfold when selecting a result.
+          attach_mappings = function(prompt_bufnr, _)
+            action_set.select:enhance({
+              post = function()
+                vim.cmd(":normal! zv")
+              end,
+            })
+            actions.center(prompt_bufnr)
+
+            return true
+          end,
+        }
+      end
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-b>"] = function(...)
+                return require("telescope.actions").delete_buffer(...)
+              end,
+              -- Add opening multi-selection support to telescope pickers
+              ["<CR>"] = multi_open,
+              ["<c-a-t>"] = actions.select_tab,
+              ["<C-j>"] = "move_selection_next",
+              ["<C-k>"] = "move_selection_previous",
+            },
+            n = {
+              ["<C-b>"] = function(...)
+                return require("telescope.actions").delete_buffer(...)
+              end,
+              -- Add opening multi-selection support to telescope pickers
+              ["<CR>"] = multi_open,
+            },
+          },
+          layout_strategy = "flex",
+          layout_config = {
+            horizontal = {
+              preview_width = 0.45,
+            },
+            vertical = {
+              width = 0.9,
+              height = 0.95,
+              preview_height = 0.5,
+              preview_cutoff = 0,
+            },
+            flex = {
+              flip_columns = 140,
+            },
+          },
+        },
+        pickers = vim.tbl_deep_extend("force", picker_config, {
+          -- Open Telescope even if there's only one result.
+          lsp_references = { jump_type = "never" },
+          lsp_definitions = { jump_type = "never" },
+          buffers = { initial_mode = "normal" },
+        }),
+        extensions = {
+          project = {
+            base_dirs = {
+              { path = "~/Desktop/Projects/" },
+              { path = "~/projects/plugins/" },
+              { path = "~/.local/share/chezmoi/" },
+              { path = "~/.local/share/nvim/" },
+            },
+          },
+          undo = {
+            side_by_side = true,
+          },
+        },
+      })
       telescope.load_extension("project")
       telescope.load_extension("zf-native")
       telescope.load_extension("undo")
