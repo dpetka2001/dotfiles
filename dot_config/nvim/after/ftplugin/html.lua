6753bef385bb506f80058fdb439a1f595fc6c820
Iordanis Petkakis <dpetka2001@gmail.com>
2023-09-09 14:15:36 +0300

    chore(treesitter): disable `mini.ai`
    Disable `mini.ai` in favor of treesitter text-objects.

--- dot_config/nvim/after/ftplugin/html.lua
+++ dot_config/nvim/after/ftplugin/html.lua
@@ -1,9 +1,9 @@
-local spec_treesitter = require("mini.ai").gen_spec.treesitter
-vim.b.miniai_config = {
-  custom_textobjects = {
-    t = spec_treesitter({
-      a = "@function.outer",
-      i = "@function.inner",
-    }),
-  },
-}
+-- local spec_treesitter = require("mini.ai").gen_spec.treesitter
+-- vim.b.miniai_config = {
+--   custom_textobjects = {
+--     t = spec_treesitter({
+--       a = "@function.outer",
+--       i = "@function.inner",
+--     }),
+--   },
+-- }
