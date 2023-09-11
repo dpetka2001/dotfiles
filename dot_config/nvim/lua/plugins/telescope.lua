3e4450bb4b656542d7a75820e02f2532873379aa
Iordanis Petkakis <dpetka2001@gmail.com>
2023-09-12 00:20:09 +0300

    fix(telescope): don't override default `config` and move everything to `opts`

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -17,23 +17,7 @@ return {
       { "natecraddock/telescope-zf-native.nvim" },
       { "debugloop/telescope-undo.nvim" },
     },
-    keys = {
-      { "<leader>sd", false },
-      { "<leader>sD", false },
-      { "<leader>sR", false },
-      { "<leader>xd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document diagnostics" },
-      { "<leader>xD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace diagnostics" },
-      { "<leader>xs", "<cmd>Telescope lsp_document_symbols<cr>", desc = "LSP Document Symbols" },
-      { "<leader>xS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "LSP Workspace Symbols" },
-      { "<leader>sM", "<cmd>Telescope man_pages sections=ALL<cr>", desc = "Man Pages" },
-      { "<leader>ff", Util.telescope("find_files"), desc = "Find files (root dir not git)" },
-      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Current Buffer Fuzzy" },
-      { "<leader>gb", "<cmd>Telescope git_bcommits<cr>", desc = "Buffer commits" },
-      { "<leader>U", "<cmd>Telescope undo<cr>", desc = "Telescope undo" },
-    },
-    -- Setup here extensions that depend on `telescope.opts`, otherwise just setup when it is called
-    config = function()
-      local telescope = require("telescope")
+    opts = function(_, opts)
       local actions = require("telescope.actions")
       local action_state = require("telescope.actions.state")
       local builtins = require("telescope.builtin")
@@ -69,64 +53,79 @@ return {
           end,
         }
       end
-
-      telescope.setup({
-        defaults = {
-          mappings = {
-            i = {
-              ["<C-b>"] = function(...)
-                return require("telescope.actions").delete_buffer(...)
-              end,
-              -- Add opening multi-selection support to telescope pickers
-              ["<CR>"] = multi_open,
-              ["<c-a-t>"] = actions.select_tab,
-              ["<C-j>"] = "move_selection_next",
-              ["<C-k>"] = "move_selection_previous",
-            },
-            n = {
-              ["<C-b>"] = function(...)
-                return require("telescope.actions").delete_buffer(...)
-              end,
-              -- Add opening multi-selection support to telescope pickers
-              ["<CR>"] = multi_open,
-            },
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+        mappings = {
+          i = {
+            ["<C-b>"] = function(...)
+              return require("telescope.actions").delete_buffer(...)
+            end,
+            -- Add opening multi-selection support to telescope pickers
+            ["<CR>"] = multi_open,
+            ["<c-a-t>"] = actions.select_tab,
+            ["<C-j>"] = "move_selection_next",
+            ["<C-k>"] = "move_selection_previous",
           },
-          layout_strategy = "flex",
-          layout_config = {
-            horizontal = {
-              preview_width = 0.45,
-            },
-            vertical = {
-              width = 0.9,
-              height = 0.95,
-              preview_height = 0.5,
-              preview_cutoff = 0,
-            },
-            flex = {
-              flip_columns = 140,
-            },
+          n = {
+            ["<C-b>"] = function(...)
+              return require("telescope.actions").delete_buffer(...)
+            end,
+            -- Add opening multi-selection support to telescope pickers
+            ["<CR>"] = multi_open,
           },
         },
-        pickers = vim.tbl_deep_extend("force", picker_config, {
-          -- Open Telescope even if there's only one result.
-          lsp_references = { jump_type = "never" },
-          lsp_definitions = { jump_type = "never" },
-          buffers = { initial_mode = "normal" },
-        }),
-        extensions = {
-          project = {
-            base_dirs = {
-              { path = "~/Desktop/Projects/" },
-              { path = "~/projects/plugins/" },
-              { path = "~/.local/share/chezmoi/" },
-              { path = "~/.local/share/nvim/" },
-            },
+        layout_strategy = "flex",
+        layout_config = {
+          horizontal = {
+            preview_width = 0.45,
           },
-          undo = {
-            side_by_side = true,
+          vertical = {
+            width = 0.9,
+            height = 0.95,
+            preview_height = 0.5,
+            preview_cutoff = 0,
+          },
+          flex = {
+            flip_columns = 140,
           },
         },
       })
+      opts.pickers = vim.tbl_deep_extend("force", picker_config, {
+        -- Open Telescope even if there's only one result.
+        lsp_references = { jump_type = "never" },
+        lsp_definitions = { jump_type = "never" },
+        buffers = { initial_mode = "normal" },
+      })
+      opts.extensions = {
+        project = {
+          base_dirs = {
+            { path = "~/Desktop/Projects/" },
+            { path = "~/projects/plugins/" },
+            { path = "~/.local/share/chezmoi/" },
+            { path = "~/.local/share/nvim/" },
+          },
+        },
+        undo = {
+          side_by_side = true,
+        },
+      }
+    end,
+    keys = {
+      { "<leader>sd", false },
+      { "<leader>sD", false },
+      { "<leader>sR", false },
+      { "<leader>xd", "<cmd>Telescope diagnostics bufnr=0<cr>", desc = "Document diagnostics" },
+      { "<leader>xD", "<cmd>Telescope diagnostics<cr>", desc = "Workspace diagnostics" },
+      { "<leader>xs", "<cmd>Telescope lsp_document_symbols<cr>", desc = "LSP Document Symbols" },
+      { "<leader>xS", "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", desc = "LSP Workspace Symbols" },
+      { "<leader>sM", "<cmd>Telescope man_pages sections=ALL<cr>", desc = "Man Pages" },
+      { "<leader>ff", Util.telescope("find_files"), desc = "Find files (root dir not git)" },
+      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Current Buffer Fuzzy" },
+      { "<leader>gb", "<cmd>Telescope git_bcommits<cr>", desc = "Buffer commits" },
+      { "<leader>U", "<cmd>Telescope undo<cr>", desc = "Telescope undo" },
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
       telescope.load_extension("project")
       telescope.load_extension("zf-native")
       telescope.load_extension("undo")
