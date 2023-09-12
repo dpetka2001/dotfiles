033e33cecc819e87cefb250201995ba3246cdffd
Iordanis Petkakis <dpetka2001@gmail.com>
2023-09-13 02:32:45 +0300

    feat(fish): replace `exa` with `eza` since former unmaintained

--- dot_config/private_fish/functions/ll.fish
+++ dot_config/private_fish/functions/ll.fish
@@ -1,3 +1,3 @@
-function ll --wraps='exa -lag' --description 'alias ll exa -lag'
-    exa -la --group-directories-first --color=always --icons $argv
+function ll --wraps='eza -lah' --description 'alias ll eza -lah'
+    eza -lah --group-directories-first --color=always --icons $argv
 end
