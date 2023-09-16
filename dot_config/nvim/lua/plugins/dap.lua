816956150d319d64b5889b93b3d18709a4aed16b
Iordanis Petkakis <dpetka2001@gmail.com>
2023-09-16 03:05:15 +0300

    feat(TS/JS): replace `typescript.nvim` with `typescript-tools` and some `dap` changes

--- dot_config/nvim/lua/plugins/dap.lua
+++ dot_config/nvim/lua/plugins/dap.lua
@@ -4,6 +4,9 @@ return {
     "mfussenegger/nvim-dap",
     -- stylua: ignore
     keys = {
+      { '<leader>dj', function() require('dap').down() end, desc = 'Down in current stacktrace' },
+      { '<leader>dk', function() require('dap').up() end, desc = 'Up in current stacktrace' },
+      { '<F5>', function() require('dap').continue() end, desc = 'Continue' },
       { "<F10>", function() require("dap").step_over() end, desc = "Step Over" },
       { "<F11>", function() require("dap").step_into() end, desc = "Step Into" },
       { "<F12>", function() require("dap").step_out() end, desc = "Step Out" },
--- dot_config/nvim/lua/plugins/extras/lang/web_dev.lua
+++ dot_config/nvim/lua/plugins/extras/lang/web_dev.lua
@@ -31,11 +31,11 @@ return {
     opts = {
       servers = {
         html = {},
-        tsserver = {
-          root_dir = function(...)
-            return require("lspconfig.util").root_pattern(".git")(...)
-          end,
-        },
+        -- tsserver = {
+        --   root_dir = function(...)
+        --     return require("lspconfig.util").root_pattern(".git")(...)
+        --   end,
+        -- },
         cssls = {},
         tailwindcss = {
           root_dir = function(...)
@@ -48,22 +48,22 @@ return {
         emmet_language_server = {},
       },
       setup = {
-        tsserver = function(_, opts)
-          require("lazyvim.util").on_attach(function(client, buffer)
-            if client.name == "tsserver" then
-              client.server_capabilities.documentFormattingProvider = false
-              -- stylua: ignore
-              vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
-              -- stylua: ignore
-              vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
-            end
-            -- if client.name == "eslint" then
-            --   client.server_capabilities.documentFormattingProvider = true
-            -- end
-          end)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
+        -- tsserver = function(_, opts)
+        --   require("lazyvim.util").on_attach(function(client, buffer)
+        --     if client.name == "tsserver" then
+        --       client.server_capabilities.documentFormattingProvider = false
+        --       -- stylua: ignore
+        --       vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
+        --       -- stylua: ignore
+        --       vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
+        --     end
+        --     -- if client.name == "eslint" then
+        --     --   client.server_capabilities.documentFormattingProvider = true
+        --     -- end
+        --   end)
+        --   require("typescript").setup({ server = opts })
+        --   return true
+        -- end,
       },
     },
   },
@@ -80,6 +80,112 @@ return {
         --   filetypes = { "html", "css", "json", "jsonc", "yaml", "markdown" },
         -- }),
       })
+    end,
+  },
+
+  -- Native TSServer client
+  {
+    "pmizio/typescript-tools.nvim",
+    event = { "BufReadPost *.ts,*.tsx,*.js,*.jsx", "BufNewFile *.ts,*.tsx,*.js,*.jsx" },
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-lspconfig" },
+    opts = {
+      -- capabilities = require("lsp").client_capabilities(),
+      -- on_attach = require("lsp").on_attach,
+      settings = {
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = "literals",
+          includeInlayVariableTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+        },
+      },
+    },
+  },
+
+  -- DAP for TS/JS
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      {
+        "rcarriga/nvim-dap-ui",
+        config = function(_, opts)
+          local dap = require("dap")
+          local dapui = require("dapui")
+
+          dapui.setup(opts)
+
+          -- Hide the fold column.
+          vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "dapui_scopes", "dapui_stacks", "dapui_breakpoints" },
+            callback = function()
+              require("ufo").detach()
+            end,
+          })
+
+          -- Automatically open the UI when a new debug session is created.
+          dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open({})
+          end
+          dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close({})
+          end
+          dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close({})
+          end
+        end,
+      },
+      {
+        "mxsdev/nvim-dap-vscode-js",
+        opts = {
+          debugger_path = vim.fn.stdpath("data") .. "/lazy/vscode-js-debug",
+          adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+        },
+      },
+      {
+        "microsoft/vscode-js-debug",
+        version = "1.x",
+        build = "npm i && npm run compile vsDebugServerBundle && mv -f dist out",
+      },
+    },
+    opts = function()
+      local dap = require("dap")
+      for _, language in ipairs({ "typescript", "javascript" }) do
+        if not dap.configurations[language] then
+          dap.configurations[language] = {
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Launch file",
+              program = "${file}",
+              cwd = "${workspaceFolder}",
+              resolveSourceMapLocations = {
+                "${workspaceFolder}/**",
+                "!**/node_modules/**",
+              },
+              outFiles = {
+                "${workspaceFolder}/**",
+                "!**/node_modules/**",
+              },
+              skipFiles = { "**/node_modules/**" },
+            },
+            {
+              type = "pwa-node",
+              request = "attach",
+              name = "Attach",
+              processId = require("dap.utils").pick_process,
+              cwd = "${workspaceFolder}",
+              resolveSourceMapLocations = {
+                "${workspaceFolder}/**",
+                "!**/node_modules/**",
+              },
+              outFiles = {
+                "${workspaceFolder}/**",
+                "!**/node_modules/**",
+              },
+              skipFiles = { "**/node_modules/**" },
+            },
+          }
+        end
+      end
     end,
   },
 }
