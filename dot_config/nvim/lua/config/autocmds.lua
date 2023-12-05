dc841b76fd7c44483fa85c2698e1a86a4f47e0ac
Iordanis Petkakis <dpetka2001@gmail.com>
2023-12-05 12:53:54 +0200

    feat(autocmd): add `ClangdAST` pattern to `lazyvim_close_with_q`

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -20,6 +20,7 @@ vim.api.nvim_create_autocmd("FileType", {
   group = "lazyvim_close_with_q",
   pattern = {
     "aerial-nav",
+    "ClangdAST",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
