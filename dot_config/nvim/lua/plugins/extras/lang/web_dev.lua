367c96c3f0b1592092d111f1cdb677f1925da5cb
Iordanis Petkakis <dpetka2001@gmail.com>
2024-01-23 11:36:26 +0200

    fix(nvim/web_dev): add `rm -rf out` to stop errors when updating `vscode-js-debug`

--- dot_config/nvim/lua/plugins/extras/lang/web_dev.lua
+++ dot_config/nvim/lua/plugins/extras/lang/web_dev.lua
@@ -161,7 +161,7 @@ return {
       {
         "microsoft/vscode-js-debug",
         version = "1.x",
-        build = "npm i && npm run compile vsDebugServerBundle && mv -f dist out",
+        build = "npm i && npm run compile vsDebugServerBundle && rm -rf out && mv -f dist out",
       },
     },
     opts = function()
