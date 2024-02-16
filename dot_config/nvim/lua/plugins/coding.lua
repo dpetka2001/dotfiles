4d47f880be012819f14be4039698315d6b125360
Iordanis Petkakis <dpetka2001@gmail.com>
2024-02-16 16:43:08 +0200

    feat(codeium): support for line/word completion and disable `nvim-cmp` autocompletion

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -79,6 +79,7 @@ return {
       opts.preselect = cmp.PreselectMode.None
       opts.completion = {
         completeopt = "menu,menuone,noinsert,noselect",
+        autocomplete = false,
       }
 
       -- Set highlight group and window options for nvim-cmp
