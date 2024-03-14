0d97fe35cd86bd6bdc8ef7bf9b04c591a1ad5ecc
Iordanis Petkakis <dpetka2001@gmail.com>
2024-03-14 19:31:44 +0200

    feat(colorscheme): generate dynamically the builtin Neovim themes

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,16 @@
 Util = require("lazyvim.util")
--- stylua: ignore
-local builtins = { "zellner", "torte", "slate", "shine", "ron", "quiet", "peachpuff",
-  "pablo", "murphy", "lunaperche", "koehler", "industry", "evening", "elflord",
-  "desert", "delek", "default", "darkblue", "blue", "morning", "retrobox", "sorbet",
-  "zaibatsu", "wildcharm", "vim", "habamax"
-}
+
 local get_colorsheme = function()
+  local builtins = {}
+
+  for name, _ in vim.fs.dir("$VIMRUNTIME/colors/") do
+    if name:match("*.txt") then
+      break
+    else
+      name = name:match("(.*).lua$") or name:match("(.*).vim$")
+      table.insert(builtins, name)
+    end
+  end
   local target = vim.fn.getcompletion
 
   ---@diagnostic disable-next-line: duplicate-set-field
