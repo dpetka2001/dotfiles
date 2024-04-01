5138053665bf77c6325a14b6c5da821ac2f6c753
Iordanis Petkakis <dpetka2001@gmail.com>
2024-04-01 19:20:23 +0300

    chore(lazygit): Keymap addition and some changes in `keymaps.lua`

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -5,25 +5,31 @@
 local map = vim.keymap.set
 -- local del = vim.keymap.del
 
---[[ Visual-multi ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                           Visual-multi                            │
+--     ╰───────────────────────────────────────────────────────────────────╯
 map("n", "<C-Left>", "<Plug>(VM-Select-h)")
 map("n", "<C-Right>", "<Plug>(VM-Select-l)")
 map("n", "<C-Up>", "<Plug>(VM-Add-Cursor-Up)")
 map("n", "<C-Down>", "<Plug>(VM-Add-Cursor-Down)")
 
---[[ Change window size ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                        Change window size                         │
+--     ╰───────────────────────────────────────────────────────────────────╯
 map("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
 map("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 map("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
 map("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 
---[[ More comfortable keys for searching history in command line ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │    More comfortable keys for searching history in command line    │
+--     ╰───────────────────────────────────────────────────────────────────╯
 map("c", "<C-j>", "<Down>", { desc = "Next command" })
 map("c", "<C-k>", "<Up>", { desc = "Previous command" })
 
--- ╭─────────────────────────────────╮
--- │ Lazygit change size and options │
--- ╰─────────────────────────────────╯
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                  Lazygit change size and options                  │
+--     ╰───────────────────────────────────────────────────────────────────╯
 -- WARN:
 -- ╭────────────────────────────────────────────────────────────────────────────╮
 -- │ Don't use `esc_esc = false` to not conflict with Lazygit <Esc> and be able │
@@ -39,8 +45,16 @@ map("n", "<leader>gf", function()
   local git_path = vim.api.nvim_buf_get_name(0)
   LazyVim.lazygit({ args = { "-f", vim.trim(git_path) }, size = { width = 1.0, height = 1.0 }, esc_esc = true })
 end, { desc = "Lazygit current file history" })
+-- INFO:
+-- ╭───────────────────────────────────────────────────────────────────╮
+-- │ Using `[[]]` instead of single/double quotes allows to not        │
+-- │ escape special characters                                         │
+-- ╰───────────────────────────────────────────────────────────────────╯
+map("t", "<Esc>", [[<C-\><C-n>0]])
 
---[[ Move to tabs convienently ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                     Move to tabs convienently                     │
+--     ╰───────────────────────────────────────────────────────────────────╯
 map("n", "<leader>1", "1gt", { desc = "Move to tab 1" })
 map("n", "<leader>2", "2gt", { desc = "Move to tab 2" })
 map("n", "<leader>3", "3gt", { desc = "Move to tab 3" })
@@ -51,15 +65,21 @@ map("n", "<leader>7", "7gt", { desc = "Move to tab 7" })
 map("n", "<leader>8", "8gt", { desc = "Move to tab 8" })
 map("n", "<leader>9", "9gt", { desc = "Move to tab 9" })
 
---[[ Close buffer and window together ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                 Close buffer and window together                  │
+--     ╰───────────────────────────────────────────────────────────────────╯
 map("n", "<leader>bw", "<cmd>execute 'bdelete' | silent! close<cr>", { desc = "Close buffer and window" })
 
---[[ Open current file in chrome ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                    Open current file in chrome                    │
+--     ╰───────────────────────────────────────────────────────────────────╯
 -- map("n", "<leader>fx", '<cmd>!google-chrome "%"<cr>', { desc = "Open file in Chrome" })
 -- stylua: ignore
 map("n", "<leader>fx", '<cmd>silent !/home/jrn23/.local/opt/firefox/firefox "%"<cr>', { silent = true, desc = "Open file in Firefox Developer" })
 
---[[ vim-tmux-navigator ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                        vim-tmux-navigator                         │
+--     ╰───────────────────────────────────────────────────────────────────╯
 if os.getenv("TMUX") then
   map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
   map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
@@ -67,12 +87,16 @@ if os.getenv("TMUX") then
   map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>")
 end
 
---[[ Open scratch ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                           Open scratch                            │
+--     ╰───────────────────────────────────────────────────────────────────╯
 map("n", "<leader>S", function()
   require("dpetka2001.scratch").launch_notepad()
 end, { silent = true, desc = "Open Scratch" })
 
---[[ Testing key overloading for speeding up macros ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │          Testing key overloading for speeding up macros           │
+--     ╰───────────────────────────────────────────────────────────────────╯
 map("n", "@", function()
   local count = vim.v.count1
   local register = vim.fn.getcharstr()
@@ -90,7 +114,9 @@ map("v", "@", function()
   vim.api.nvim_command("silent update")
 end, { noremap = true })
 
---[[ Harpoon mappings ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                         Harpoon mappings                          │
+--     ╰───────────────────────────────────────────────────────────────────╯
 -- stylua: ignore
 if vim.bo["filetype"] ~= "rnvimr" then
   map("n", "<M-1>", function() require("harpoon.ui").nav_file(1) end, { desc = "Harpoon 1" })
@@ -105,10 +131,14 @@ if vim.bo["filetype"] ~= "rnvimr" then
   map("n", "<M-0>", function() require("harpoon.ui").nav_file(0) end, { desc = "Harpoon 0" })
 end
 
---[[ Visual Selection Formatting ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                    Visual Selection Formatting                    │
+--     ╰───────────────────────────────────────────────────────────────────╯
 map("v", "<leader>ct", "<cmd>lua vim.lsp.buf.format({async=true})<cr>", { desc = "Visual Formatting" })
 
---[[ Word/Line Completion with Codeium ]]
+--     ╭───────────────────────────────────────────────────────────────────╮
+--     │                 Word/Line Completion with Codeium                 │
+--     ╰───────────────────────────────────────────────────────────────────╯
 -- See https://github.com/Exafunction/codeium.vim/issues/27#issuecomment-1948401179 for info
 local expr_opts = {
   noremap = true,
