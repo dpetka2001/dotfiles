1f1376350f62f515149b5a0ea21b24066896df01
Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>
2024-05-19 14:16:35 +0300

    feat(fish/nvims): take into account configs in `~/.config/test_configs`

--- dot_config/private_fish/functions/nvims.fish
+++ dot_config/private_fish/functions/nvims.fish
@@ -1,6 +1,9 @@
 function nvims
     set items nvim LazyVimDev
-    set config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
+    for item in (ls -d ~/.config/test_configs/*)
+        set -a items $(string match -rg '.*/(.*/.*$)' $item)
+    end
+    set config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --cycle --border --exit-0)
     if test -z $config
         echo "Nothing selected"
         return 0
