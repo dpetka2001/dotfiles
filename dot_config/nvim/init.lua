951bbf9b3ca11a4da270de0e663aea1b8f9f9c8b
Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>
2024-05-25 12:17:02 +0300

    chore: remove `globals` since LazyVim incorporated `has`

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,4 +5,3 @@
 
 vim.loader.enable()
 require("config.lazy")
-require("globals")
--- dot_config/nvim/lua/globals/init.lua
+++
@@ -1,4 +0,0 @@
--- function to return the spec of a plugin
-SPEC = function(name)
-  return require("lazy.core.config").spec.plugins[name]
-end
