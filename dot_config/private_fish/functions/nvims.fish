729ca3c83ed6bb121f511c048b44d2d02e0120ed
Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>
2024-10-05 11:24:34 +0300

    chore: properly grab only directories in `nvims` and use LazyVimDev as default git editor

--- dot_config/private_fish/functions/nvims.fish
+++ dot_config/private_fish/functions/nvims.fish
@@ -1,7 +1,7 @@
 function nvims
     set items nvim LazyVimDev
-    for item in (ls -d ~/.config/test_configs/*)
-        set -a items $(string match -rg '.*/(.*/.*$)' $item)
+    for item in (ls -d ~/.config/test_configs/*/)
+        set -a items $(string match -rg '.*/(.*/.*)/$' $item)
     end
     set config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
     if test -z $config
