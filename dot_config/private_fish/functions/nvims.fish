ff4b53a0340f3d2c90a9b27c0a23bce9f2ce79f8
Iordanis Petkakis <12776461+dpetka2001@users.noreply.github.com>
2025-03-19 17:44:08 +0200

    fix(fish): make `nvims` function more robust wrt glob patterns warnings

--- dot_config/private_fish/functions/nvims.fish
+++ dot_config/private_fish/functions/nvims.fish
@@ -1,6 +1,15 @@
 function nvims
     set items nvim LazyVimDev
-    for item in (ls -d ~/.config/test_configs/**/ 2>/dev/null | string match -rg '.*(test_configs/.*)/$')
+    # assign glob pattern to a variable so that no warnings are shown
+    # see also `help wildcards-globbing` which mentions
+    # INFO:
+    # ╭────────────────────────────────────────────────────────────────────╮
+    # │ There are exceptions, namely set and path, overriding variables in │
+    # │ overrides, count and for. Their globs will instead expand to zero  │
+    # │ arguments (so the command won’t see them at all)                   │
+    # ╰────────────────────────────────────────────────────────────────────╯
+    set glob ~/.config/test_configs/*/
+    for item in (ls -d $glob 2>/dev/null | string match -rg '.*(test_configs/.*)/$')
         set -a items $item
     end
     set config (printf "%s\n" $items | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
